d70a524db4ace6e1f80af488a29418d58d91d417
Zack <zalecknero@gmail.com>
2025-05-14 03:26:42 +0800

    Added proper ui config to the masterPage and added code to the buttons to redirect

--- CaseStudy/UserSetupInterface.Master.cs
+++ CaseStudy/UserSetupInterface.Master.cs
@@ -11,7 +11,22 @@ namespace CaseStudy
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RegistrationTab.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("LoginTab.aspx");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminLog.aspx");
         }
     }
 }
--- CaseStudy/UserSetupInterface.Master.designer.cs
+++ CaseStudy/UserSetupInterface.Master.designer.cs
@@ -51,6 +51,15 @@ namespace CaseStudy
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
         /// Panel2 control.
         /// </summary>
         /// <remarks>
@@ -58,6 +67,33 @@ namespace CaseStudy
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Panel Panel2;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// Button3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button3;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
